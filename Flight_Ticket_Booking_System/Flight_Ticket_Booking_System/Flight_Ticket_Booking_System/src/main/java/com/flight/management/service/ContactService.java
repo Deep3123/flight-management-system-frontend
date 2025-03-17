184032e7e49e6fbee2997bc7f84e185ce935b976
Deep <kronosdp3123@gmail.com>
2025-03-18 02:12:38 +0530

    Contact us module completed

--- Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/ContactService.java
+++ Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/ContactService.java
@@ -9,17 +9,9 @@ public interface ContactService {
 
 	public List<ContactProxy> getAllContactUsDetails();
 
-<<<<<<< HEAD:Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/ContactService.java
 	public List<ContactProxy> getAllContactUsDetailsByName(String name);
 
 //	public String updateContactUsDetails(ContactProxy contactProxy);
 
 //	public String deleteContactUsDetails(String name);
-=======
-	public ContactProxy getAllContactUsDetailsByName(String name);
-
-	public String updateContactUsDetails(ContactProxy contactProxy);
-
-	public String deleteContactUsDetails();
->>>>>>> 7b91b131e662fc3e7296914c4ac0bc88a3f9a0e6:Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/ContactService.java
 }
--- Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/impl/ContactServiceImpl.java
+++ Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/impl/ContactServiceImpl.java
@@ -1,11 +1,7 @@
 package com.flight.management.service.impl;
 
 import java.util.List;
-<<<<<<< HEAD:Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/impl/ContactServiceImpl.java
 import java.util.Optional;
-=======
->>>>>>> 7b91b131e662fc3e7296914c4ac0bc88a3f9a0e6:Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/impl/ContactServiceImpl.java
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.flight.management.domain.ContactEntity;
@@ -33,7 +29,6 @@ public class ContactServiceImpl implements ContactService {
 	}
 
 	@Override
-<<<<<<< HEAD:Flight_Ticket_Booking_System/Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/impl/ContactServiceImpl.java
 	public List<ContactProxy> getAllContactUsDetailsByName(String name) {
 		// TODO Auto-generated method stub
 		Optional<List<ContactEntity>> list = repo.findByName(name);
@@ -61,23 +56,4 @@ public class ContactServiceImpl implements ContactService {
 //		// TODO Auto-generated method stub
 //		return null;
 //	}
-=======
-	public ContactProxy getAllContactUsDetailsByName(String name) {
-		// TODO Auto-generated method stub
-		return MapperUtil.convertValue(repo.findByName(name).get(), ContactProxy.class);
-	}
-
-	@Override
-	public String updateContactUsDetails(ContactProxy contactProxy) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String deleteContactUsDetails() {
-		// TODO Auto-generated method stub
-		return null;
-	}
->>>>>>> 7b91b131e662fc3e7296914c4ac0bc88a3f9a0e6:Flight_Ticket_Booking_System/src/main/java/com/flight/management/service/impl/ContactServiceImpl.java
-
 }
