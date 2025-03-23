de11a8a2ee8bdb7b0551d0f3f233d53b2ed84c9c
Deep <kronosdp3123@gmail.com>
2025-03-24 02:05:28 +0530

    Removing all files from GitHub repository

--- frontend/flight_ticket_booking_system/src/app/dtoClasses/login-req.ts
+++
@@ -1,3 +0,0 @@
-export class LoginReq {
-    constructor(public username?: any, public password?: any) { }
-}
--- frontend/flight_ticket_booking_system/src/app/dtoClasses/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  constructor(
-    public name?: any,
-    public emailId?: any,
-    public mobileNo?: any,
-    public username?: any,
-    public password?: any
-  ) {}
-}
