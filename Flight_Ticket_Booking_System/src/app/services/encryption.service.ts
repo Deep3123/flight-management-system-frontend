f6b96df0648c8c1a6e5b5f43ed0b3c16f5471186
Deep Patel <deep.patel@oplinnovate.com>
2025-04-28 18:21:36 +0530

    Environment variable created.

--- Flight_Ticket_Booking_System/src/app/services/encryption.service.ts
+++ Flight_Ticket_Booking_System/src/app/services/encryption.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import * as CryptoJS from "crypto-js";
 import { environment } from "../../environments/environment";
+import { environment_prod } from "../../environments/environment.prod.template";
 
 @Injectable({
   providedIn: "root",
@@ -14,10 +15,15 @@ export class EncryptionService {
   //   process.env["ENCRYPTION_IV"] || ""
   // );
 
+  // private readonly SECRET_KEY = CryptoJS.enc.Utf8.parse(
+  //   environment.encryption.secretKey
+  // );
+  // private readonly IV = CryptoJS.enc.Utf8.parse(environment.encryption.iv);
+
   private readonly SECRET_KEY = CryptoJS.enc.Utf8.parse(
-    environment.encryption.secretKey
+    environment_prod.encryption.secretKey
   );
-  private readonly IV = CryptoJS.enc.Utf8.parse(environment.encryption.iv);
+  private readonly IV = CryptoJS.enc.Utf8.parse(environment_prod.encryption.iv);
 
   encrypt(data: string): string {
     const encrypted = CryptoJS.AES.encrypt(data, this.SECRET_KEY, {
--- Flight_Ticket_Booking_System/src/environments/environment.prod.template.ts
+++ Flight_Ticket_Booking_System/src/environments/environment.prod.template.ts
@@ -1,4 +1,4 @@
-export const environment = {
+export const environment_prod = {
   production: true,
   encryption: {
     secretKey: "${ENCRYPTION_SECRET_KEY}",
