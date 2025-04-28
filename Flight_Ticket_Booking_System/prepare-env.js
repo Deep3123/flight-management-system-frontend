891f3ebdeefd079d39ef8fa0a140cc00d36d26ce
Deep <kronosdp3123@gmail.com>
2025-04-28 23:33:01 +0530

    Configuration changed.

--- Flight_Ticket_Booking_System/prepare-env.js
+++ Flight_Ticket_Booking_System/prepare-env.js
@@ -6,8 +6,8 @@ const template = fs.readFileSync('./src/environments/environment.prod.template.t
 
 // Replace placeholders with environment variables or default values
 const output = template
-    .replace('${ENCRYPTION_SECRET_KEY}', process.env.ENCRYPTION_SECRET_KEY || 'x2B7eTf93mQ9cGzYdFk7pLm8XsRjHtNv')
-    .replace('${ENCRYPTION_IV}', process.env.ENCRYPTION_IV || '7fH1d9Lm3cQ5x7Vz  ');
+    .replace('${ENCRYPTION_SECRET_KEY}', process.env.ENCRYPTION_SECRET_KEY || '')
+    .replace('${ENCRYPTION_IV}', process.env.ENCRYPTION_IV || '');
 
 // Make sure the environments directory exists
 if (!fs.existsSync('./src/environments')) {
