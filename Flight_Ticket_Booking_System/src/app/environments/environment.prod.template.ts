c744eca7e20210fb3d0f5100f7c7845508bda619
Deep Patel <deep.patel@oplinnovate.com>
2025-04-28 18:09:06 +0530

    Environment variable created.

--- Flight_Ticket_Booking_System/src/app/environments/environment.prod.template.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  encryption: {
-    secretKey: "${ENCRYPTION_SECRET_KEY}",
-    iv: "${ENCRYPTION_IV}",
-  },
-};
--- Flight_Ticket_Booking_System/src/app/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  encryption: {
-    secretKey: "x2B7eTf93mQ9cGzYdFk7pLm8XsRjHtNv",
-    iv: "7fH1d9Lm3cQ5x7Vz",
-  },
-};
