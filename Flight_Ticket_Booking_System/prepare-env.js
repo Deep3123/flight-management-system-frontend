309d73857222eea1064e7d4d68edddb89e3bfece
Deep <kronosdp3123@gmail.com>
2025-05-03 00:35:25 +0530

    Configuration changed.

--- Flight_Ticket_Booking_System/prepare-env.js
+++ Flight_Ticket_Booking_System/prepare-env.js
@@ -10,25 +10,28 @@ const defaultEnv = `export const environment = {
   encryption: {
     secretKey: "${process.env.ENCRYPTION_SECRET_KEY || ''}",
     iv: "${process.env.ENCRYPTION_IV || ''}",
+    googleClientId: "${process.env.SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_ID || ''}",
   },
 };`;
 
 // Get environment variables
 const secretKey = process.env.ENCRYPTION_SECRET_KEY || '';
 const iv = process.env.ENCRYPTION_IV || '';
+const googleClientId = process.env.SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_ID || ''
 
 if (!secretKey || !iv) {
-    console.warn('Warning: One or more encryption environment variables are not set.');
+  console.warn('Warning: One or more encryption environment variables are not set.');
 }
 
 // Replace placeholders with environment variables
 const prodOutput = prodTemplate
-    .replace('${ENCRYPTION_SECRET_KEY}', secretKey)
-    .replace('${ENCRYPTION_IV}', iv);
+  .replace('${ENCRYPTION_SECRET_KEY}', secretKey)
+  .replace('${ENCRYPTION_IV}', iv)
+  .replace('${SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_GOOGLE_CLIENT_ID}', googleClientId)
 
 // Make sure the environments directory exists
 if (!fs.existsSync('./src/environments')) {
-    fs.mkdirSync('./src/environments', { recursive: true });
+  fs.mkdirSync('./src/environments', { recursive: true });
 }
 
 // Write both environment files
